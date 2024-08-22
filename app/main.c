c0604a3be4ff803a5a99bbf3bfc561a6af785859
ShivamBajpai04 <shivambajpai04@gmail.com>
2024-08-22 23:02:22 +0530

    add REPL and exit 0 command

--- app/main.c
+++ app/main.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main() {
-  printf("$ ");
-  fflush(stdout);
+  while(1){//REPL (Read-Eval-Print-Loop)
+    printf("$ ");
+    fflush(stdout);//clear stdout stream
 
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
+    // Wait for user input
+    char input[100];
+    fgets(input, 100, stdin);
 
-  input[strlen(input)-1]='\0';
-  printf("%s: command not found\n",input);
+    input[strlen(input)-1]='\0'; //replacing default \n terminator with \0
+    //exit
+    if(!strcmp(input,"exit 0")){
+      exit(0);
+    }
+    printf("%s: command not found\n",input);
+  }
   return 0;
 }
