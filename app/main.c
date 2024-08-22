a3fca4e7248b38386d72f3fd79199cdd01fd4668
ShivamBajpai04 <shivambajpai04@gmail.com>
2024-08-23 01:30:24 +0530

    add pwd and cd command

--- app/main.c
+++ app/main.c
@@ -2,6 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#ifdef _WIN32
+#include<io.h>
+// #include <direct.h>
+// #define getcwd _getcwd
+// #define chdir _chdir
+#else
+#include <unistd.h>
+#endif
+
+#define MAX_INPUT_SIZE 100
+#define MAX_TOKENS 10
 
 bool is_builtin(char* command) {
     char* commands[] = {"cd", "echo", "exit", "pwd", "type"};
@@ -14,33 +25,74 @@ bool is_builtin(char* command) {
     return false;
 }
 
+void handle_builtin(char** args) {
+    if (strcmp(args[0], "exit") == 0) { // exit
+        exit(0);
+    } else if (strcmp(args[0], "echo") == 0) { // echo
+        for (int j = 1; args[j] != NULL; j++) {
+            printf("%s ", args[j]);
+        }
+        printf("\n");
+    } else if (strcmp(args[0], "pwd") == 0) { // pwd
+        char cwd[1024];
+        if (getcwd(cwd, sizeof(cwd)) != NULL) {
+            printf("%s\n", cwd);
+        } else {
+            perror("pwd failed");
+        }
+    } else if (strcmp(args[0], "cd") == 0) { // cd
+        if (args[1] == NULL) {
+            fprintf(stderr, "cd: missing argument\n");
+        } else {
+            if (chdir(args[1]) != 0) {
+                perror("cd failed");
+            }
+        }
+    } else if (strcmp(args[0], "type") == 0) { // type
+        if (args[1] != NULL && is_builtin(args[1])) {
+            printf("%s is a builtin command\n", args[1]);
+        } else {
+            printf("%s: command not found\n", args[1] ? args[1] : "type");
+        }
+    }
+}
+
 int main() {
+
+        
     while (1) { // REPL (Read-Eval-Print-Loop)
-        printf("$ ");
+        char cwd[1024];
+        getcwd(cwd, sizeof(cwd));
+        printf("%s $ ",cwd);
         fflush(stdout); // clear stdout stream
 
         // Wait for user input
-        char input[100];
-        fgets(input, 100, stdin);
+        char input[MAX_INPUT_SIZE];
+        fgets(input, MAX_INPUT_SIZE, stdin);
 
-        input[strlen(input) - 1] = '\0'; // replacing default \n terminator with \0 as it messes up the command not found output by making the message appear in the next line
+        input[strlen(input) - 1] = '\0'; // replacing default \n terminator with \0
+
+        // Skip empty input
         if (strlen(input) == 0) {
-          continue;
+            continue;
         }
-        if (!strncmp(input, "exit", 4)) { // exit
-            return 0;
-        } else if (!strncmp(input, "echo ", 5)) { //echo
-            printf("%s\n", input + 5);
-        } else if (!strncmp(input, "type ", 5)) { //type
-            if (is_builtin(input + 5)) {
-                printf("%s is a builtin command\n", input + 5);
-                continue;
-            } else {
-                printf("%s: command not found\n", input + 5);
-                continue;
-            }
-        } else { //invalid command
-            printf("%s: command not found\n", input);
+
+        // Tokenize input
+        char* token = strtok(input, " ");
+        char* args[MAX_TOKENS];
+        int i = 0;
+
+        while (token != NULL && i < MAX_TOKENS - 1) {
+            args[i++] = token;
+            token = strtok(NULL, " ");
+        }
+        args[i] = NULL; // NULL-terminate the array of tokens
+
+        // Check if the command is a built-in
+        if (is_builtin(args[0])) {
+            handle_builtin(args);
+        } else {
+            printf("%s: command not found\n", args[0]);
         }
     }
     return 0;
